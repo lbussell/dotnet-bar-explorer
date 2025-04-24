f0793c2437cc922f50142bdc32b4cb70fb31a00d
Logan Bussell <loganbussell@microsoft.com>
2025-04-24 09:36:53 -0700

    Serialize build

--- BarExplorer/BuildCommand.cs
+++ BarExplorer/BuildCommand.cs
@@ -1,10 +1,12 @@
+using System.Text.Json;
 using BarExplorer.Cli;
 using Microsoft.DotNet.DarcLib;
+using Microsoft.DotNet.ProductConstructionService.Client.Models;
 using Microsoft.Extensions.Logging;
 
 namespace BarExplorer;
 
-internal partial class BuildCommand(
+internal class BuildCommand(
     IBasicBarClient barClient,
     ILogger<BuildCommand> logger)
     : BaseCommand<BuildOptions>
@@ -12,19 +14,49 @@ internal partial class BuildCommand(
     private readonly IBasicBarClient _barClient = barClient;
     private readonly ILogger<BuildCommand> _logger = logger;
 
+    private static readonly JsonSerializerOptions s_jsonOptions = new()
+    {
+        WriteIndented = true,
+    };
+
     public override async Task<int> ExecuteAsync(BuildOptions options)
     {
         _logger.LogInformation("Getting build {options.Id}", options.Id);
 
         var build = await _barClient.GetBuildAsync(options.Id);
+        var buildResult = new BuildResult(build);
+        var buildJson = JsonSerializer.Serialize(buildResult, s_jsonOptions);
 
         _logger.LogInformation(
             """
             Build {options.Id}:
-            {build}
+            {buildJson}
             """,
-            options.Id, build);
+            options.Id, buildJson);
 
         return 0;
     }
+}
+
+// Custom wrapper to exclude Assets because the array can be really large
+internal class BuildResult(Build build)
+{
+    public int Id { get; } = build.Id;
+    public string Commit { get; } = build.Commit;
+    public int? AzureDevOpsBuildId { get; } = build.AzureDevOpsBuildId;
+    public int? AzureDevOpsBuildDefinitionId { get; } = build.AzureDevOpsBuildDefinitionId;
+    public string AzureDevOpsAccount { get; } = build.AzureDevOpsAccount;
+    public string AzureDevOpsProject { get; } = build.AzureDevOpsProject;
+    public string AzureDevOpsBuildNumber { get; } = build.AzureDevOpsBuildNumber;
+    public string AzureDevOpsRepository { get; } = build.AzureDevOpsRepository;
+    public string AzureDevOpsBranch { get; } = build.AzureDevOpsBranch;
+    public string GitHubRepository { get; } = build.GitHubRepository;
+    public string GitHubBranch { get; } = build.GitHubBranch;
+    public DateTimeOffset DateProduced { get; } = build.DateProduced;
+    public List<Channel> Channels { get; } = build.Channels;
+    public List<BuildRef> Dependencies { get; } = build.Dependencies;
+    public List<BuildIncoherence> Incoherencies { get; } = build.Incoherencies;
+    public int Staleness { get; } = build.Staleness;
+    public bool Released { get; } = build.Released;
+    public bool Stable { get; } = build.Stable;
 }
